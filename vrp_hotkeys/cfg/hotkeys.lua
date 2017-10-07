8f1d95eb375fa9be455f9843b70fae408636260c
Sighmir <guistoppa1995@hotmail.com>
2017-10-06 23:27:32 -0300

    Minor fixes + Crouch and Point
    Minor fixes + Crouch and Point

--- vrp_hotkeys/cfg/hotkeys.lua
+++ vrp_hotkeys/cfg/hotkeys.lua
@@ -9,16 +9,19 @@ vRP = Proxy.getInterface("vRP")
 -- GLOBAL VARIABLES
 player = GetPlayerPed(-1)
 handsup = false
+crouched = false
+pointing = false
 
 -- YOU ARE ON A CLIENT SCRIPT ( Just reminding you ;) )
 -- Keys IDs can be found at https://wiki.fivem.net/wiki/Controls
 
 -- Hotkeys Configuration: cfg.hotkeys = {[Key] = {group = 1, pressed = function() end, released = function() end},}
 cfg.hotkeys = {
-  [323] = {
-    -- X toggle HandsUp as Example!
+  [73] = {
+    -- X Toggle HandsUp
     group = 1, 
 	pressed = function() 
+      --if not IsPauseMenuActive() and not IsPedInAnyVehicle(player, true) then -- Uncomment to block use in vehicle
 	  handsup = not handsup
 	  SetEnableHandcuffs(player, handsup)
 	  if handsup then
@@ -26,6 +29,59 @@ cfg.hotkeys = {
 	  else
 	    vRP.stopAnim({true})
 		SetPedStealthMovement(player,false,"") 
+	  end
+	  --end -- Uncomment to block use in vehicle
+	end,
+	released = function()
+	  --[[ Remove the []s to use on hold
+      --if not IsPauseMenuActive() and not IsPedInAnyVehicle(player, true) then -- Uncomment to block use in vehicle
+	  handsup = not handsup
+	  vRP.stopAnim({true})
+	  SetPedStealthMovement(player,false,"") 
+	  --end -- Uncomment to block use in vehicle
+	  ]]
+	end,
+  },
+  [29] = {
+    -- Hold B to Point
+    group = 1, 
+	pressed = function() 
+      --if not IsPauseMenuActive() and not IsPedInAnyVehicle(player, true) then -- Uncomment to block use in vehicle
+	  pointing = not pointing
+	  SetEnableHandcuffs(player, pointing)
+	  if pointing then
+	    vRP.playAnim({true,{{"oddjobs@hunter", "point_fwd", 1}},true})
+	  else
+	    vRP.stopAnim({true})
+		SetPedStealthMovement(player,false,"") 
+	  end
+	  --end -- Uncomment to block use in vehicle
+	end,
+	released = function()
+	  -- Remove the []s to use on hold
+      --if not IsPauseMenuActive() and not IsPedInAnyVehicle(player, true) then -- Uncomment to block use in vehicle
+	  pointing = not pointing
+	  vRP.stopAnim({true})
+	  SetPedStealthMovement(player,false,"") 
+	  --end -- Uncomment to block use in vehicle
+	  
+	end,
+  },
+  [36] = {
+    --CTRL for Crouch
+    group = 0, 
+	pressed = function() 
+      if not IsPauseMenuActive() and not IsPedInAnyVehicle(player, true) then 
+        RequestAnimSet("move_ped_crouched")
+		while not HasAnimSetLoaded("move_ped_crouched") do 
+          Citizen.Wait(100)
+        end 
+        crouched = not crouched 
+		if crouched then 
+          ResetPedMovementClipset(player, 0)
+        else
+          SetPedMovementClipset(player, "move_ped_crouched", 0.25)
+        end 
 	  end
 	end,
 	released = function()
--- vrp_hotkeys/client.lua
+++ vrp_hotkeys/client.lua
@@ -1,26 +1,36 @@
+-- MAIN THREAD
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
 		for k,v in pairs(cfg.hotkeys) do
-		  if IsControlPressed(v.group, k) then
+		  if IsControlJustPressed(v.group, k) or IsDisabledControlJustPressed(v.group, k) then
 		    v.pressed()
 		  end
 
-		  if IsControlReleased(v.group, k) then
+		  if IsControlJustReleased(v.group, k) or IsDisabledControlJustReleased(v.group, k) then
 		    v.released()
 		  end
 		end
 	end
 end)
 
--- HERE YOU CAN CREATE NECESSARY FUNCTIONS
+-- OTHER THREADS
+-- THIS IS FOR CROUCH
+Citizen.CreateThread(function()
+  while true do 
+    Citizen.Wait(0)
+    if DoesEntityExist(player) and not IsEntityDead(player) then 
+      DisableControlAction(0,36,true) -- INPUT_DUCK   
+    end 
+  end
+end)
 
 -- THIS IS FOR HANDSUP
 Citizen.CreateThread(function()
   while true do
     Citizen.Wait(0)
     if handsup then
-      SetPedStealthMovement(GetPlayerPed(-1),true,"")
+      SetPedStealthMovement(player,true,"")
       DisableControlAction(0,21,true) -- disable sprint
       DisableControlAction(0,24,true) -- disable attack
       DisableControlAction(0,25,true) -- disable aim
@@ -38,3 +48,29 @@ Citizen.CreateThread(function()
     end
   end
 end)
+
+-- THIS IS FOR POINTING
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(0)
+    if pointing then
+      SetPedStealthMovement(player,true,"")
+      DisableControlAction(0,21,true) -- disable sprint
+      DisableControlAction(0,24,true) -- disable attack
+      DisableControlAction(0,25,true) -- disable aim
+      DisableControlAction(0,47,true) -- disable weapon
+      DisableControlAction(0,58,true) -- disable weapon
+      DisableControlAction(0,263,true) -- disable melee
+      DisableControlAction(0,264,true) -- disable melee
+      DisableControlAction(0,257,true) -- disable melee
+      DisableControlAction(0,140,true) -- disable melee
+      DisableControlAction(0,141,true) -- disable melee
+      DisableControlAction(0,142,true) -- disable melee
+      DisableControlAction(0,143,true) -- disable melee
+      DisableControlAction(0,75,true) -- disable exit vehicle
+      DisableControlAction(27,75,true) -- disable exit vehicle
+    end
+  end
+end)
+
+-- OTHER FUNCTIONS
