49d08eb9185c491f64800763cd351859137a9470
Sighmir <guistoppa1995@hotmail.com>
2017-10-07 19:54:02 -0300

    Fixed Pointing, Improved Handsup and Crouch
    Fixed Pointing, Improved Handsup and Crouch
    
    Note: Use GetPlayerPed(-1) every time instead of "player", because that get's only the first Ped the player get!

--- vrp_hotkeys/cfg/hotkeys.lua
+++ vrp_hotkeys/cfg/hotkeys.lua
@@ -7,9 +7,9 @@ HKserver = Tunnel.getInterface("vrp_hotkeys","vrp_hotkeys")
 vRP = Proxy.getInterface("vRP")
 
 -- GLOBAL VARIABLES
-player = GetPlayerPed(-1)
 handsup = false
 crouched = false
+pointing = false
 
 -- YOU ARE ON A CLIENT SCRIPT ( Just reminding you ;) )
 -- Keys IDs can be found at https://wiki.fivem.net/wiki/Controls
@@ -20,15 +20,47 @@ cfg.hotkeys = {
     -- X toggle HandsUp
     group = 1, 
 	pressed = function() 
-      if not IsPauseMenuActive() and not IsPedInAnyVehicle(player, true) then -- Comment to allow use in vehicle
+      if not IsPauseMenuActive() and not IsPedInAnyVehicle(GetPlayerPed(-1), true) then -- Comment to allow use in vehicle
 	    handsup = not handsup
-	    SetEnableHandcuffs(player, handsup)
+	    SetEnableHandcuffs(GetPlayerPed(-1), handsup)
 	    if handsup then
 	      vRP.playAnim({true,{{"random@mugging3", "handsup_standing_base", 1}},true})
 	    else
 	      vRP.stopAnim({true})
-		  SetPedStealthMovement(player,false,"") 
+		  SetPedStealthMovement(GetPlayerPed(-1),false,"") 
 	    end
+	  end -- Comment to allow use in vehicle
+	end,
+	released = function()
+	  -- Do nothing on release because it's toggle.
+	end,
+  },
+  [29] = {
+    -- B toggle Point
+    group = 0, 
+	pressed = function() 
+      if not IsPauseMenuActive() and not IsPedInAnyVehicle(GetPlayerPed(-1), true) then  -- Comment to allow use in vehicle
+		RequestAnimDict("anim@mp_point")
+		while not HasAnimDictLoaded("anim@mp_point") do
+          Wait(0)
+		end
+        pointing = not pointing 
+		if pointing then 
+		  SetPedCurrentWeaponVisible(GetPlayerPed(-1), 0, 1, 1, 1)
+		  SetPedConfigFlag(GetPlayerPed(-1), 36, 1)
+		  Citizen.InvokeNative(0x2D537BA194896636, GetPlayerPed(-1), "task_mp_pointing", 0.5, 0, "anim@mp_point", 24)
+		  RemoveAnimDict("anim@mp_point")
+        else
+		  Citizen.InvokeNative(0xD01015C7316AE176, GetPlayerPed(-1), "Stop")
+		  if not IsPedInjured(GetPlayerPed(-1)) then
+		    ClearPedSecondaryTask(GetPlayerPed(-1))
+		  end
+		  if not IsPedInAnyVehicle(GetPlayerPed(-1), 1) then
+		    SetPedCurrentWeaponVisible(GetPlayerPed(-1), 1, 1, 1, 1)
+		  end
+		  SetPedConfigFlag(GetPlayerPed(-1), 36, 0)
+		  ClearPedSecondaryTask(PlayerPedId())
+        end 
 	  end -- Comment to allow use in vehicle
 	end,
 	released = function()
@@ -39,16 +71,16 @@ cfg.hotkeys = {
     -- CTRL toggle Crouch
     group = 0, 
 	pressed = function() 
-      if not IsPauseMenuActive() and not IsPedInAnyVehicle(player, true) then  -- Comment to allow use in vehicle
+      if not IsPauseMenuActive() and not IsPedInAnyVehicle(GetPlayerPed(-1), true) then  -- Comment to allow use in vehicle
         RequestAnimSet("move_ped_crouched")
 		while not HasAnimSetLoaded("move_ped_crouched") do 
           Citizen.Wait(100)
         end 
         crouched = not crouched 
 		if crouched then 
-          ResetPedMovementClipset(player, 0)
+          ResetPedMovementClipset(GetPlayerPed(-1), 0)
         else
-          SetPedMovementClipset(player, "move_ped_crouched", 0.25)
+          SetPedMovementClipset(GetPlayerPed(-1), "move_ped_crouched", 0.25)
         end 
 	  end -- Comment to allow use in vehicle
 	end,
--- vrp_hotkeys/client.lua
+++ vrp_hotkeys/client.lua
@@ -19,7 +19,7 @@ end)
 Citizen.CreateThread(function()
   while true do 
     Citizen.Wait(0)
-    if DoesEntityExist(player) and not IsEntityDead(player) then 
+    if DoesEntityExist(GetPlayerPed(-1)) and not IsEntityDead(GetPlayerPed(-1)) then 
       DisableControlAction(0,36,true) -- INPUT_DUCK   
     end 
   end
@@ -30,14 +30,16 @@ Citizen.CreateThread(function()
   while true do
     Citizen.Wait(0)
     if handsup then
-      SetPedStealthMovement(player,true,"")
+      SetPedStealthMovement(GetPlayerPed(-1),true,"")
       DisableControlAction(0,21,true) -- disable sprint
       DisableControlAction(0,24,true) -- disable attack
       DisableControlAction(0,25,true) -- disable aim
       DisableControlAction(0,47,true) -- disable weapon
       DisableControlAction(0,58,true) -- disable weapon
-      DisableControlAction(27,133,true) -- veh turn left
-      DisableControlAction(27,134,true) -- veh turn left
+      DisableControlAction(0,71,true) -- veh forward
+      DisableControlAction(0,72,true) -- veh backwards
+      DisableControlAction(0,63,true) -- veh turn left
+      DisableControlAction(0,64,true) -- veh turn right
       DisableControlAction(0,263,true) -- disable melee
       DisableControlAction(0,264,true) -- disable melee
       DisableControlAction(0,257,true) -- disable melee
@@ -47,6 +49,44 @@ Citizen.CreateThread(function()
       DisableControlAction(0,143,true) -- disable melee
       DisableControlAction(0,75,true) -- disable exit vehicle
       DisableControlAction(27,75,true) -- disable exit vehicle
+    end
+  end
+end)
+
+-- THIS IS FOR HANDSUP
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(0)
+    if pointing then
+      local camPitch = GetGameplayCamRelativePitch()
+      if camPitch < -70.0 then
+          camPitch = -70.0
+      elseif camPitch > 42.0 then
+          camPitch = 42.0
+      end
+      camPitch = (camPitch + 70.0) / 112.0
+
+      local camHeading = GetGameplayCamRelativeHeading()
+      local cosCamHeading = Cos(camHeading)
+      local sinCamHeading = Sin(camHeading)
+      if camHeading < -180.0 then
+          camHeading = -180.0
+      elseif camHeading > 180.0 then
+          camHeading = 180.0
+      end
+      camHeading = (camHeading + 180.0) / 360.0
+
+      local blocked = 0
+      local nn = 0
+
+      local coords = GetOffsetFromEntityInWorldCoords(GetPlayerPed(-1), (cosCamHeading * -0.2) - (sinCamHeading * (0.4 * camHeading + 0.3)), (sinCamHeading * -0.2) + (cosCamHeading * (0.4 * camHeading + 0.3)), 0.6)
+      local ray = Cast_3dRayPointToPoint(coords.x, coords.y, coords.z - 0.2, coords.x, coords.y, coords.z + 0.2, 0.4, 95, GetPlayerPed(-1), 7);
+      nn,blocked,coords,coords = GetRaycastResult(ray)
+
+      Citizen.InvokeNative(0xD5BB4025AE449A4E, GetPlayerPed(-1), "Pitch", camPitch)
+      Citizen.InvokeNative(0xD5BB4025AE449A4E, GetPlayerPed(-1), "Heading", camHeading * -1.0 + 1.0)
+      Citizen.InvokeNative(0xB0A6CFD2C69C1088, GetPlayerPed(-1), "isBlocked", blocked)
+      Citizen.InvokeNative(0xB0A6CFD2C69C1088, GetPlayerPed(-1), "isFirstPerson", Citizen.InvokeNative(0xEE778F8C7E1142E2, Citizen.InvokeNative(0x19CAFA3C87F7C2FF)) == 4)
     end
   end
 end)
